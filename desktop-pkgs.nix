45691b26dc29e76f074dfc699499ad2a62a7eb66
Nick Bathum <nickbathum@gmail.com>
2021-12-14 16:34:21 -0500

    Use psi-notify as systemd service

--- desktop-pkgs.nix
+++ desktop-pkgs.nix
@@ -1,13 +1,14 @@
 { config, pkgs, ... }:
 
 {
+  systemd.packages = with pkgs; [
+    psi-notify  # manually configure for now
+  ];
+    
   environment.systemPackages = with pkgs; [
     maim
     vlc
     dia
-
-    # This should be a service?
-    psi-notify  # manually configure for now
 
     obs-studio
 
