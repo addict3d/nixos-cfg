91636ac3522b206a340b59ae5f233b07d277b96f
Nick Bathum <nickbathum@gmail.com>
2022-08-18 14:15:56 -0400

    maybe fix printer

--- printer.nix
+++ printer.nix
@@ -1,9 +1,17 @@
 { config, pkgs, ... }:
 
+let
+  myhplip = pkgs.hplip.override {
+    withPlugin = true;
+    withStaticPPDInstall = true;
+  };
+in
 {
+  environment.systemPackages = [ myhplip ];
+
   services.printing = {
     enable = true;
-    drivers = with pkgs; [ hplip ];
+    drivers = [ myhplip ];
   };
 }
 
