613c67a78ac452fa5b8095653f00039938945fe8
Nick Bathum <nickbathum@gmail.com>
2023-01-05 22:42:47 -0500

    my repl script: use '--file' parameter

--- repl.sh
+++ repl.sh
@@ -1,1 +1,1 @@
-source /etc/set-environment && nix repl $(echo $NIX_PATH | rg '^.*?repl=(/nix/store/[a-z0-9]{32}-source/repl.nix).*?$' -r '$1')
+source /etc/set-environment && nix repl --file $(echo $NIX_PATH | rg '^.*?repl=(/nix/store/[a-z0-9]{32}-source/repl.nix).*?$' -r '$1')
