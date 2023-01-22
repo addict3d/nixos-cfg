7d183e79dedda609aab95b39671999fbf97cefec
Nick Bathum <nickbathum@gmail.com>
2023-01-22 14:59:10 -0500

    yeah no

--- flake.nix
+++ flake.nix
@@ -16,24 +16,7 @@
   };
 
   outputs = inputs: 
-let
-        it87-overlay = self: super: {
-          # derp. namespaced under linuxPackages
-          linuxPackages = super.linuxPackages.extend (lpself: lpsuper: {
-            it87 = super.linuxPackages.it87.overrideAttrs (old: {
-              src = super.fetchFromGitHub {
-                owner = "frankcrawford";
-                repo = "it87";
-                # Jan 4th, 2023, Updated to match naming convention used in-tree version
-                rev = "3e5333ad0b85216a81f33692875f33456e8064c9";
-                sha256 = "sha256-wVhs//iwZUUGRTk1DpV/SnA7NZ7cFyYbsUbtazlxb6Q=";
-              };
-            });
-          });
-        };
-      in
-
-
+ 
   rec {
     # nixosModules = import ./nixos/modules;
     formatter.x86_64-linux = inputs.nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
@@ -44,7 +27,7 @@ let
       {
         system = "x86_64-linux";
         modules = [
-          ({ config, pkgs, ... }: { nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlay it87-overlay ]; })
+          ({ config, pkgs, ... }: { nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlay ]; })
           inputs.musnix.nixosModules.musnix
           ./configuration.nix ## Configuration file from regular /etc/nixos config
 
--- kernel.nix
+++ kernel.nix
@@ -1,10 +1,24 @@
 { config, pkgs, ... }:
 
+let
+  my-linuxPackages = pkgs.linuxPackages_6_0;
+  my-it87 = my-linuxPackages.it87.overrideAttrs (old: {
+    src = pkgs.fetchFromGitHub {
+      owner = "frankcrawford";
+      repo = "it87";
+      # Jan 4th, 2023, Updated to match naming convention used in-tree version
+      rev = "3e5333ad0b85216a81f33692875f33456e8064c9";
+      sha256 = "sha256-wVhs//iwZUUGRTk1DpV/SnA7NZ7cFyYbsUbtazlxb6Q=";
+    };
+  });
+in
+
+
 {
   boot = {
-    kernelPackages = pkgs.linuxPackages_6_0;
+    kernelPackages = my-linuxPackages;
     kernelModules = [ "it87" ];
-    extraModulePackages = with config.boot.kernelPackages; [ it87 v4l2loopback ];
+    extraModulePackages = with config.boot.kernelPackages; [ my-it87 v4l2loopback ];
   };
 
  # musnix.kernel = {
