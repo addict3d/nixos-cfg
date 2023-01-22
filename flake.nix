933d11e94698e7859f8489bb8278268823574635
Nick Bathum <nickbathum@gmail.com>
2023-01-21 21:18:05 -0500

    yeah

--- flake.nix
+++ flake.nix
@@ -19,14 +19,16 @@
 let
         it87-overlay = self: super: {
           # derp. namespaced under linuxPackages
-          it87 = super.it87.overrideAttrs (old: {
-            src = super.fetchFromGitHub {
-              owner = "frankcrawford";
-              repo = "it87";
-              # Jan 4th, 2023, Updated to match naming convention used in-tree version
-              rev = "3e5333ad0b85216a81f33692875f33456e8064c9";
-              sha256 = "sha256-wVhs//iwZUUGRTk1DpV/SnA7NZ7cFyYbsUbtazlxb6Q=";
-            };
+          linuxPackages = super.linuxPackages.extend (lpself: lpsuper: {
+            it87 = super.linuxPackages.it87.overrideAttrs (old: {
+              src = super.fetchFromGitHub {
+                owner = "frankcrawford";
+                repo = "it87";
+                # Jan 4th, 2023, Updated to match naming convention used in-tree version
+                rev = "3e5333ad0b85216a81f33692875f33456e8064c9";
+                sha256 = "sha256-wVhs//iwZUUGRTk1DpV/SnA7NZ7cFyYbsUbtazlxb6Q=";
+              };
+            });
           });
         };
       in
