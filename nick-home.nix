ee5c487b974b9d30235efa837a4daec61f2feaea
Nick Bathum <nickbathum@gmail.com>
2024-08-20 14:33:04 -0400

    enable git-lfs, allow myself to override nix daemon settings

--- nick-home.nix
+++ nick-home.nix
@@ -138,6 +138,8 @@
   programs.git = {
     enable = true;
 
+    lfs.enable = true;
+
     userName = "Nick Bathum";
     userEmail = "nickbathum@gmail.com";
 
--- nix.nix
+++ nix.nix
@@ -2,7 +2,13 @@
 
 {
   nix = {
-    settings.auto-optimise-store = true;
+    settings = {
+      auto-optimise-store = true;
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
+    };
     package = pkgs.nixVersions.latest;
     extraOptions = ''
       experimental-features = nix-command flakes
