d4fb8974a31d0478135acb008b892e016f2cfef3
Nick Bathum <nickbathum@gmail.com>
2026-04-13 10:31:40 -0400

    nit: fix deprecated 'system' reference

--- desktop-pkgs.nix
+++ desktop-pkgs.nix
@@ -7,7 +7,7 @@
 
   programs.neovim = {
     enable = true;
-    package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
+    package = inputs.neovim-nightly-overlay.packages.${pkgs.stdenvNoCC.hostPlatform.system}.default;
     viAlias = true;
     vimAlias = true;
 
